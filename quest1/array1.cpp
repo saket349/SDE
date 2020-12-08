17f697c7cfa06d354de00676e2e35ff5c537e2a2
Saket Kumar <saket.1999bksc@gmai.com>
2020-12-09 02:23:28 +0530

    change

---
+++ quest1/array1.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long 
+#define ull unsigned long long
+#define ld long double
+
+#define pb push_back
+#define si(x)	scanf("%d",&x)
+#define fo(i,n) for(int i=0;i<n;i++)
+#define foc(i,k,n) for(int i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+//==================================================================================
+const int mod = 1000000007;
+//==================================================================================
+//template<typename T>
+
+int main()
+{
+ios_base::sync_with_stdio(0); 
+cin.tie(0); cout.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        
+    }
+return 0;
+}
