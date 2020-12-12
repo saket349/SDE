4e8a1b5e55d40e120bbc2ecc264117af0ef80063
Saket Kumar <62437314+saket349@users.noreply.github.com>
2020-12-12 15:18:43 +0530

    Update method2.cpp

--- Day1_array/quest6/method2.cpp
+++ Day1_array/quest6/method2.cpp
@@ -1,6 +1,16 @@
 // A C++ program for merging overlapping intervals 
-#include<bits/stdc++.h> 
-using namespace std; 
+#define ll long long 
+#define ull unsigned long long
+#define ld long double
+
+#define pb push_back
+#define si(x)	scanf("%d",&x)
+#define fo(i,n) for(int i=0;i<n;i++)
+#define foc(i,k,n) for(int i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+//==================================================================================
+const int mod = 1000000007;
+//==================================================================================
+//template<typename T>
 
 // An interval has start time and end time 
 struct Interval 
