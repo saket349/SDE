0944fb05b06028b247c537155946539b41b6a6a2
Saket Kumar <saket.1999bksc@gmai.com>
2021-01-11 21:31:01 +0530

    updated

---
+++ Day17_BinaryTree/quest4_leftView/recursion.cpp
@@ -0,0 +1,37 @@
+/* A binary tree node
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+ */
+
+void solve(Node* root, int level,int *max_level,vector<int> &v){
+    if(root!=NULL)
+    {
+        if(*max_level < level)
+        {
+            v.push_back(root->data);
+            *max_level=level;
+        }
+        
+        solve(root->left,level+1,max_level,v);
+        solve(root->right,level+1,max_level,v);
+    }
+}
+vector<int> leftView(Node *root)
+{
+   // Your code here
+   Node* p = root;
+   int max_level = 0;
+   vector<int> v;
+   solve(p,1,&max_level,v);
+   return v;
+}
