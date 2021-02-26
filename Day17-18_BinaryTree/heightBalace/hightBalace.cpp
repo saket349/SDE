3724f6e2ff2c8b5cc23fd84488837cc41b5fee69
Saket Kumar <saket.1999bksc@gmai.com>
2021-02-26 19:51:08 +0530

    file added

---
+++ Day17-18_BinaryTree/heightBalace/hightBalace.cpp
@@ -0,0 +1,11 @@
+/*
+    a height-balanced binary tree is defined as:
+
+a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+
+*/
+
+/*
+    same as diameter, just in this case we donot save the max lenght instead compare left and right height ;
+    if difference is more than 1 then mark ans as false
+*/
