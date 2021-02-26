ac55c2f1446748842dd96771be47bad70b3f916e
Saket Kumar <62437314+saket349@users.noreply.github.com>
2021-02-26 19:52:24 +0530

    Update hightBalace.cpp

--- Day17-18_BinaryTree/heightBalace/hightBalace.cpp
+++ Day17-18_BinaryTree/heightBalace/hightBalace.cpp
@@ -9,3 +9,42 @@ a binary tree in which the left and right subtrees of every node differ in heigh
     same as diameter, just in this case we donot save the max lenght instead compare left and right height ;
     if difference is more than 1 then mark ans as false
 */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+int ans;
+int solve(TreeNode* node){
+    if(node!=NULL){
+        int l=0,r=0;
+        if(node->left!=NULL)
+           l = solve(node->left);
+        if(node->right!=NULL)
+           r = solve(node->right);
+        
+        if(abs(l-r)>1)
+            ans=0;
+        
+        return max(l+1,r+1);
+    }
+    return 0;
+}
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        ans=1;
+        solve(root);
+        if(ans==1)
+            return true;
+        else
+            return false;
+    }
+};
