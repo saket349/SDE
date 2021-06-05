6423d7ecd2d4b3f1d19ec2b480f7d9ac85274b2d
Saket Kumar <62437314+saket349@users.noreply.github.com>
2021-06-05 14:22:22 +0530

    Update iterative.cpp

--- BST/populate_nextRight_pointer/iterative.cpp
+++ BST/populate_nextRight_pointer/iterative.cpp
@@ -16,39 +16,30 @@ public:
 };
 */
 
+# we do simple things here, use queue and do level wise traversal, while traversing a row, we already had it stored in queue
+# we start with first element, then move along the row, while doing this we stores their both child in queue.
+
 class Solution {
 public:
     Node* connect(Node* root) {
         queue<Node*> q;
-        
-        if(root!=NULL){
-            q.push(root);
-            
-            while(q.empty()!=true){
-                
-                int n=q.size();
-                Node* p = q.front();
-                
-                if(p->left!=NULL)
-                    q.push(p->left);
-                if(p->right!=NULL)
-                    q.push(p->right);
+        if(root==NULL) return root;
+        q.push(root);
+        while(!q.empty()){
+            Node* p = q.front();
+            q.pop();
+            int n = q.size();
+            if(p->left!=NULL) q.push(p->left);
+            if(p->right!=NULL) q.push(p->right);
+            while(n--){
+                Node* t = q.front();
                 q.pop();
-                n--;
-                
-                while(n>0){
-                    Node* t = q.front();
-                    p->next = t;
-                    p=t;
-                    if(p->left!=NULL)
-                        q.push(p->left);
-                    if(p->right!=NULL)
-                        q.push(p->right);
-                    q.pop();
-                    n--;
-                }
-                p->next = NULL;
+                p->next = t;
+                p = t;
+                if(p->left!=NULL) q.push(p->left);
+                if(p->right!=NULL) q.push(p->right);
             }
+            p->next = NULL;
         }
         return root;
     }
