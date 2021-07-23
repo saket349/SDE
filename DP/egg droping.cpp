f071e98337cd87b0a23d305a62be794487ccc9e6
Saket Kumar Singh <62437314+saket349@users.noreply.github.com>
2021-07-23 18:37:37 +0530

    Create egg droping.cpp

---
+++ DP/egg droping.cpp
@@ -0,0 +1,18 @@
+vector<vector<int>> dp;
+    int solve(int f, int e){
+        if(f==0 || f==1) return f;
+        if(e==1) return f;
+        if(dp[f][e]!=-1) return dp[f][e];
+        int mn = INT_MAX;
+        for(int k=1; k<=f;k++){
+            int temp = 1 + max(solve(k-1, e-1), solve(f-k, e));
+            mn = min(mn, temp);
+        }
+        return dp[f][e] = mn;
+    }
+    int eggDrop(int n, int k) 
+    {
+        dp = vector<vector<int>> (k+1, vector<int> (n+1, -1));
+        // your code here
+        return solve(k, n);
+    }
